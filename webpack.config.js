9f6e32c3990ad59eeee3021aa29afad353f610b3
Glitch (voting-app-fcc) <none>
2018-09-13 06:53:33 +0000

    🏠🌅 Mocha and chai test

--- webpack.config.js
+++ webpack.config.js
@@ -7,10 +7,10 @@ module.exports = {
   entry: './app/app.jsx',
   output: {
     path: path.join(__dirname, 'public'),
-    filename: '[name].bundle.js',
+    filename: '[name].bundle.js'
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.scss'],
+    extensions: ['.js', '.jsx', '.scss']
   },
   module: {
     rules: [
